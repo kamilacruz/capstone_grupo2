37910b464de3efcec3d4f8d578fbb737e97c1d9c
Cristian Arévalo <cristian.arevalo@uc.cl>
2025-05-21 18:37:44 -0400

    Agregada Imagen preliminar de 100 iteraciones

--- Capstone E2/main.py
+++ Capstone E2/main.py
@@ -1,4 +1,5 @@
 # main.py
+import pandas as pd
 import matplotlib.pyplot as plt
 from modelo import correr_modelo
 
@@ -7,6 +8,10 @@ if __name__ == "__main__":
     for iteracion in range(100):
         utilidad = correr_modelo()
         utilidades.append(utilidad)
+    # Creación del df
+    df_utilidad = pd.DataFrame(utilidades)
+    print("Resumen de la utilidad:")
+    print(df_utilidad.describe())
     plt.hist(utilidades, color = 'navy', ec = 'black')
     plt.xlabel('Suma de utilidades entre ambas tiendas')
     plt.ylabel('Frecuencia')
